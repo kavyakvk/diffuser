detach to fix gradient errors

diffuser/models/temporal.py
@@ -239,7 +239,7 @@ class TemporalLSTMMemoryUnet(nn.Module):
         t = self.time_mlp(time)
 
         obs = cond[0]
-        out, self.lstm_state = self.lstm(obs.unsqueeze(-1), self.lstm_state)
+        out, self.lstm_state = self.lstm(obs.unsqueeze(-1), self.lstm_state.detach())
         # TODO(kavyakvk): use out for a action grounding loss with the previous action 
         memory = torch.concat((
             self.lstm_state[0].squeeze(dim=0),