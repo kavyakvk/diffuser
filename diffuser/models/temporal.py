fixing gradient issues

diffuser/models/temporal.py
@@ -221,8 +221,8 @@ class TemporalLSTMMemoryUnet(nn.Module):
             num_layers=1,
             batch_first=True)
         self.lstm_state = (
-            torch.randn(1, batch_size, lstm_hidden_size),
-            torch.randn(1, batch_size, lstm_hidden_size))
+            torch.randn(1, batch_size, lstm_hidden_size, requires_grad=True),
+            torch.randn(1, batch_size, lstm_hidden_size, requires_grad=True))
         
         self.action_grounding_loss = action_grounding_loss
 