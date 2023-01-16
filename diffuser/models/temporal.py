d28406d7c9c69798303cc0e34ccc4f484aa5c33b
Kavya Kopparapu <kavyakvk@gmail.com>
2023-01-15 19:33:35 -0500

    Detach issues

--- diffuser/models/temporal.py
+++ diffuser/models/temporal.py
@@ -239,7 +239,8 @@ class TemporalLSTMMemoryUnet(nn.Module):
         t = self.time_mlp(time)
 
         obs = cond[0]
-        out, self.lstm_state = self.lstm(obs.unsqueeze(-1), self.lstm_state.detach())
+        lstm_state_detached = (s.detach() for s in self.lstm_state)
+        out, self.lstm_state = self.lstm(obs.unsqueeze(-1), lstm_state_detached)
         # TODO(kavyakvk): use out for a action grounding loss with the previous action 
         memory = torch.concat((
             self.lstm_state[0].squeeze(dim=0),
